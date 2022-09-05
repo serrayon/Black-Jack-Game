fc95d7775ee1a186ceb208707962f9331d66aab6
Serrayon <serrayon81@gmail.com>
2022-09-04 20:58:19 -0700

    added buttons

--- Black Jack Game/GameScene.swift
+++ Black Jack Game/GameScene.swift
@@ -1,88 +1,62 @@
-//
-//  GameScene.swift
-//  Black Jack Game
-//
-//  Created by Octavio Serrano on 9/4/22.
-//
-
 import SpriteKit
 import GameplayKit
-
+ 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    let moneyContainer = SKSpriteNode(color: .clear, size: CGSize(width:250, height: 150))
+        let dealBtn = SKSpriteNode(imageNamed: "deal_btn")
+        let hitBtn = SKSpriteNode(imageNamed: "hit_btn")
+        let standBtn = SKSpriteNode(imageNamed: "stand_btn")
+        let money10 = Money(moneyValue: .ten)
+        let money25 = Money(moneyValue: .twentyFive)
+        let money50 = Money(moneyValue: .fifty)
+        let instructionText = SKLabelNode(text: "Place your bet")
+     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        setupTable()
+        setupMoney()
+        setupButtons()
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func setupTable(){
+        let table = SKSpriteNode(imageNamed: "table")
+        addChild(table)
+        table.position = CGPoint(x: size.width/2, y: size.height/2)
+        table.zPosition = -1
+        addChild(moneyContainer)
+        moneyContainer.anchorPoint = CGPoint(x:0, y:0)
+        moneyContainer.position = CGPoint(x:size.width/2 - 125, y:size.height/2)
+        instructionText.fontColor = UIColor.black
+        addChild(instructionText)
+        instructionText.position = CGPoint(x: size.width/2, y: 400)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    func setupMoney(){
+            addChild(money10)
+            money10.position = CGPoint(x: 75, y: 40)
+             
+            addChild(money25)
+            money25.position = CGPoint(x:130, y:40)
+             
+            addChild(money50)
+            money50.position = CGPoint(x: 185, y:40)
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func setupButtons(){
+        dealBtn.name = "dealBtn"
+        addChild(dealBtn)
+        dealBtn.position = CGPoint(x:300, y:40)
+             
+        hitBtn.name = "hitBtn"
+        addChild(hitBtn)
+        hitBtn.position = CGPoint(x:450, y:40)
+        hitBtn.isHidden = true
+             
+        standBtn.name = "standBtn"
+        addChild(standBtn)
+        standBtn.position = CGPoint(x:600, y:40)
+        standBtn.isHidden = true
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
--- Black Jack Game/GameViewController.swift
+++ Black Jack Game/GameViewController.swift
@@ -1,48 +1,20 @@
-//
-//  GameViewController.swift
-//  Black Jack Game
-//
-//  Created by Octavio Serrano on 9/4/22.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
-
+ 
 class GameViewController: UIViewController {
-
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+         
+        let scene = GameScene(size:CGSize(width: 768, height: 1024))
+        let skView = self.view as! SKView
+        skView.showsFPS = false
+        skView.showsNodeCount = false
+        skView.ignoresSiblingOrder = false
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
+     
     override var prefersStatusBarHidden: Bool {
         return true
     }
